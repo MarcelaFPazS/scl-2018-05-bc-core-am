3c7fedd6e5a315e329c37fa8c46a76801f830e19
Marcela Paz <marcela.fpaz.s@gmail.com>
2018-05-30 22:17:09 -0400

    arreglos de identacion, primeros codigos en js, arreglo de botones

--- src/cipher.js
+++ src/cipher.js
@@ -6,3 +6,26 @@ window.cipher = {
     /* Acá va tu código */
   }
 }
+
+
+const encode = () =>{
+  const name = " aqui va el nombre la const"
+  let result="";
+  for (let i=0; i<name.length; i++){
+    var textChar = ((name.charCodeAt(i) - 65 +3) %26) + 65;
+    var textCipher = string.fromCharCode(textChar);
+
+  }
+}
+
+
+
+const decode = () =>{
+  const name = "lala"
+  let result="";
+  for (let i=0; i<name.length; i++){
+    var textChar = ((name.charCodeAt(i) - 65 -3) %26) + 65;
+    var textCipher = string. fromCharCode(textChar);
+    
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 /* Acá va tu código */
+/*  primera función para ingresar nombre de la persona y recibir instrucciones*/
 function Function (){
-    var user = document.getElementById("name").value;
-    var p = document.getElementById("welcomeMsje2");
-    p.innerHTML =   user + "  escribe codifica y decodifica... ahora nadie sabra tus secretos,El cifrado César mueve cada letra un determinado número de espacios en el alfabeto. En este ejemplo se usa un desplazamiento de tres espacios, así que una B en el texto original se convierte en una E en el texto codificado. ";
+  var user = document.getElementById("name").value;
+  var p = document.getElementById("welcomeMsje2");
+  p.innerHTML =   user + "  escribe codifica y decodifica... ahora nadie sabra tus secretos,El cifrado César mueve cada letra un determinado número de espacios en el alfabeto. En este ejemplo se usa un desplazamiento de tres espacios, así que una B en el texto original se convierte en una E en el texto codificado. ";
 }
 
 
 function Function2 (){
-    var user = document.getElementById("data").value;
-    var p = document.getElementById("decifrar");
-    p.innerHTML =   user + " ";
+  var user = document.getElementById("data").value;
+  var p = document.getElementById("decifrar");
+  p.innerHTML =   user + " ";
 }
   
